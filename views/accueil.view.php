3143453211123e130adf1bf98190fead316eee1c
Klariane <eniky2005@yahoo.fr>
2021-10-12 20:33:32 +0200

    updatedu 12.10.2021_amélioration de la page d'accueil!

--- views/accueil.view.php
+++ views/accueil.view.php
@@ -4,7 +4,25 @@
 ob_start();
 ?>
 <!--Je peux mettre du html ici no stress-->
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BiBiothèque MGA</title>
+    <link rel="stylesheet" href="public/CSS/style.css">
+</head>
+<body>
+    <header id="showcase">
 
+    <h1>Bienvenue à la bibliothèque MGA</h1>
+    
+    <a href="<?= URL ?>livres" class="button">En savoir plus...</a>
+    </header>
+    
+</body>
+</html>
 
 
 
@@ -12,8 +30,8 @@ ob_start();
 
 <?php 
 
-$content = ob_get_clean(); // ici je deverse tout ce qui va etre entre ob_start et ob_get_clean
-$titre = "Bibliothèque MGA";
-require "template.php";
+// $content = ob_get_clean(); // ici je deverse tout ce qui va etre entre ob_start et ob_get_clean
+// $titre = "Bibliothèque MGA";
+// require "template.php";
 
 ?>
